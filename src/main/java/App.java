5beec3ce93fd4311c2fbcf56c3ed5e9cb7e0cc50
Svetlana Ilina <developersvil@gmail.com>
2023-04-25 12:46:09 +0300

    first

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,22 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat beancat =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beancat.getName());
+
+        Cat beancat2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beancat2.getName());
+
+        System.out.println(bean.equals(bean2));
+        System.out.println(beancat.equals(beancat2));
+
+
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,3 +1,4 @@
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -10,5 +11,12 @@ public class AppConfig {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+    @Bean(name="cat")
+    @Scope(BeanDefinition.SCOPE_PROTOTYPE)
+    public Cat getCat() {
+        Cat cat = new Cat();
+        cat.setName("I am a cat!");
+        return cat;
     }
 }
